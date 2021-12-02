5345c2c32693d80a813e429271d73bc3bf2c801b
Harut Safaryan <safaryanharut@gmail.com>
2021-12-02 22:53:23 +0400

    Add project files.

---
+++ Delegates/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Account
+    {
+        private int _sum;
+        public delegate void AccountStateHandler(string message);
+        AccountStateHandler _del;
+
+        public void RegisterHndler(AccountStateHandler del)
+        {
+            _del += del;
+        }
+
+        public void UnregisterHandler(AccountStateHandler del)
+        {
+            _del -= del;
+        }
+
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+        public int CurrentSum { get { return _sum; } }
+        public void Put(int sum) 
+        {
+            _sum += sum; 
+        }
+        public void Withdraw(int sum)
+        {
+            if (sum < _sum)
+            {
+                _sum -= sum;
+                if (_del != null)
+                    _del($"The amount {sum} has been withdraw from account ");
+            }
+            else
+            {
+                if (_del != null)
+                    _del($"There are not enough money");
+            }
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(200);
+            Account.AccountStateHandler colorDelegate = new Account.AccountStateHandler(ColorMessage);
+            
+            account.RegisterHndler(new Account.AccountStateHandler(ShowMessage));
+            account.RegisterHndler(colorDelegate);
+
+            account.Withdraw(110);
+            account.Withdraw(100);
+
+            account.UnregisterHandler(colorDelegate);
+            account.Withdraw(50);
+
+            Console.ReadLine();
+        }
+
+        private static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void ColorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+    }
+}

b670c71063b6bbdcdb86c6cce12e5c4af726c9bf
Harut Safaryan <safaryanharut@gmail.com>
2021-12-03 17:01:14 +0400

    stock

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Delegates
 {
@@ -8,7 +9,7 @@ namespace Delegates
         {
             Account account = new Account(200);
             Account.AccountStateHandler colorDelegate = new Account.AccountStateHandler(ColorMessage);
-            
+
             account.RegisterHndler(new Account.AccountStateHandler(ShowMessage));
             account.RegisterHndler(colorDelegate);
 
@@ -18,7 +19,26 @@ namespace Delegates
             account.UnregisterHandler(colorDelegate);
             account.Withdraw(50);
 
-            Console.ReadLine();
+
+            Console.WriteLine("\n\n\nStock operations");
+
+            List<Item> list = new List<Item>()
+            {
+                new Item(521, "pen", 20),
+                new Item(220, "book", 30),
+                new Item(300, "notebook", 40)
+            };
+            
+            Stock stock = new Stock(list);
+            Stock.StockOpertation QuantityMessage = new Stock.StockOpertation(ColorMessage);
+            stock.RegisterHandler(QuantityMessage);
+            
+            stock.AddQty(521, 100);
+            stock.TakOfQty(220, 28);
+            
+            stock.UnregisterHandler(QuantityMessage);
+
+            stock.AddQty(300, 80);
         }
 
         private static void ShowMessage(string message)
@@ -30,6 +50,7 @@ namespace Delegates
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
---
+++ Delegates/Stock.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Item
+    {
+        public Item(int article, string name, int qty)
+        {
+            Article = article;
+            Name = name;
+            Qty = qty;
+        }
+        public int Article { get; set; }
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public const int MinQty = 5;
+        public const int MaxQty = 100;
+    }
+    public class Stock
+    {
+        List<Item> _items = new List<Item>();
+        public Stock(List<Item> items)
+        {
+            _items = items;
+        }
+
+        public delegate void StockOpertation(string message);
+        StockOpertation _del;
+
+        public void RegisterHandler(StockOpertation del)
+        {
+            _del += del;
+        }
+
+        public void UnregisterHandler(StockOpertation del)
+        {
+            _del -= del;
+        }
+
+        public void AddQty(int article, int qty)
+        {
+            Item currentItem = _items.FirstOrDefault(x => x.Article == article);
+            currentItem.Qty += qty;
+
+            if (currentItem.Qty > Item.MaxQty)
+                _del?.Invoke($"The Item {currentItem.Name} rich the maximum qty");
+        }
+
+        public void TakOfQty(int article, int qty)
+        {
+            Item currentItem = _items.FirstOrDefault(x => x.Article == article);
+            currentItem.Qty -= qty;
+
+            if (currentItem.Qty < Item.MinQty)
+                _del?.Invoke($"The Item {currentItem.Name} rich the minimum qty");
+        }
+    }
+}
